5e91dd08cf27dbf1c8f56ac2988bf38ca5630511
Hwang Seong Taek <79950091+6uamy@users.noreply.github.com>
2022-08-15 18:09:44 +0900

    Update 2480.js

--- basic/조건문/2480.js
+++ basic/조건문/2480.js
@@ -24,3 +24,39 @@ if(dice1 == dice2 && dice2 == dice3 && dice1 == dice3) {
 }
 
 console.log(prize);
+
+
+
+
+// 화살표 함수를 활용한 풀이
+const fs = require('fs');
+
+let input = fs.readFileSync('/dev/stdin').toString();
+
+input = input.split(' ');
+
+const dice1 = +input[0];
+const dice2 = +input[1];
+const dice3 = +input[2];
+
+let prize = 0;
+
+const result = (a, b, c) => {
+    if(dice1 == dice2 && dice2 == dice3 && dice1 == dice3) {
+        prize = 10000 + dice1 * 1000;
+        return console.log(prize);
+    } else if(dice1 == dice2 || dice2 == dice3 || dice1 == dice3) {
+        if(dice1 == dice2 || dice1 == dice3) {
+            prize = 1000 + dice1 * 100;
+            return console.log(prize);
+        } else {
+            prize = 1000 + dice2 * 100;
+            return console.log(prize);
+        }        
+    } else {
+        prize = Math.max(dice1, dice2, dice3) * 100;
+        return console.log(prize);
+    }   
+}
+
+result(dice1, dice2, dice3);
