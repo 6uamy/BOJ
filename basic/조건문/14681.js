4151b44e7f98b00b3a11deb4650e90843e5efd96
Hwang Seong Taek <79950091+6uamy@users.noreply.github.com>
2022-08-15 15:43:32 +0900

    Create 14681.js

---
+++ basic/조건문/14681.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+let input = fs.readFileSync('0').toString(); 
+// 원래는 이부분에 '/dev/stdin'으로 입력하였으나 런타임 에러가 발생해 0으로 적어주니 해결되었다.
+
+input = input.split('\n');
+
+const x = +input[0];
+const y = +input[1];
+
+if(x > 0 && y > 0) {
+    console.log(1)
+} else if(x < 0 && y > 0) {
+    console.log(2)
+} else if(x < 0 && y < 0) {
+    console.log(3)
+} else {
+    console.log(4)
+}
