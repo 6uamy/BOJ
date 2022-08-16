5ab47cdb7d9482baad5b17be648b16c5f4c8472e
Hwang Seong Taek <79950091+6uamy@users.noreply.github.com>
2022-08-16 21:13:35 +0900

    Update 2739.js

--- basic/반복문/2739.js
+++ basic/반복문/2739.js
@@ -1,11 +1,8 @@
 const fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString();
+let input = fs.readFileSync("./index.txt").toString();
 
 input = +input;
 
-let result = 0;
-
 for(let i=1; i<10; i++){
-    result = input * i;
-    console.log(`${input} * ${i} = ${result}`);
+    console.log(`${input} * ${i} = ${input * i}`);
 }
