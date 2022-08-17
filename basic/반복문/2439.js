cee112664a84b512455c46623adb8ba7aee75aa8
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2022-08-18 00:31:27 +0900

    Create 2439.js

---
+++ basic/반복문/2439.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString();
+
+let num = +input;
+
+for (let i = 0; i < num; i++) {
+    let star = '';
+    for (let j = num - 1; j >= 0; j--) {
+      star += j <= i ? '*' : ' ';
+    }
+    console.log(star);
+}
