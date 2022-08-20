8c8411a702ec51aeb9d555788efb6b5386510394
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2022-08-20 18:40:48 +0900

    Create 3052.js

---
+++ basic/배열/3052.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+
+let count = input.split('\n').map(x=>Number(x));
+let rem = count.map(x => x % 42);
+
+const result = new Set(rem); // Set 객체를 통해 중복 값을 제거한 뒤 배열을 재구성 해준다.
+
+console.log(result.size); // 중복 값이 제거된 배열의 사이즈를 출력
