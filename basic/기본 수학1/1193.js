3b0f17d00ed298431a9b7f017ed7ad3162a28200
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2022-08-30 21:30:14 +0900

    Create 1193.js

---
+++ basic/기본 수학1/1193.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+  
+let number = +input;
+let value = 1;
+while (true) {
+	number -= value;
+	if (number <= 0) {
+		number += value;
+		break;
+	}
+	value++;
+}
+
+if (value % 2 === 1) {
+	console.log(`${value - (number - 1)}/${1 + (number - 1)}`);
+} else {
+	console.log(`${1 + (number - 1)}/${value - (number - 1)}`);
+}
