cdea5cdf44e279cf6a8926080957ce455f43346f
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2022-09-10 23:16:03 +0900

    Create 2750.js

---
+++ basic/정렬/2750.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let testCase = +input.shift();
+
+const sorted = input.sort((a, b) => a-b); 
+
+for(let i=0; i<testCase; i++){
+    console.log(sorted[i])
+}
