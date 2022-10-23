82159e5b1f71b8cdf2699894bf5eb346480bb785
Hwang Seongtaek <79950091+6uamy@users.noreply.github.com>
2022-10-23 23:57:43 +0900

    Create 2164.js

---
+++ basic/기본 수학1/2164.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+
+const solution = (n) => {
+	//숫자를 2진법으로 변환
+	const binArr = n.toString(2).split("");
+    
+  //n보다 작은 2^x 값 빼기 
+	binArr.shift();
+    
+	//남은수 10진법으로 변환
+	const answer = parseInt(binArr.join(""), 2);
+    
+    //answer가 0이면 그 수는 2^x 이므로 n
+    return answer ? answer * 2 : n;
+}
+
+console.log(solution(Number(input));
